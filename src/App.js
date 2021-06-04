ba05a4c789fa1e75df491327b7c7215559156fa8
Annemarie Wisbar <a.wisbar@gmail.com>
2021-06-04 16:02:20 +0200

    Create example medication array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-import "./App.css";
+import React from 'react'
 
 export default function App() {
-  return <div className="App"></div>;
+  const medication = [
+    {
+      time: '8:00',
+      meds: [
+        { id: 1, medName: 'Spironolacton' },
+        { id: 2, medName: 'Enalapril' },
+        { id: 3, medName: 'Prednisolon' },
+      ],
+    },
+    { time: '9:00', meds: [{ id: 1, medName: 'Tacrolimus' }] },
+    {
+      time: '13:00',
+      meds: [
+        { id: 1, medName: 'Magnesium' },
+        { id: 2, medName: 'MMF' },
+      ],
+    },
+    {
+      time: '18:00',
+      meds: [
+        { id: 1, medName: 'Magnesium' },
+        { id: 2, medName: 'MMF' },
+      ],
+    },
+    { time: '21:00', meds: [{ id: 1, medName: 'Tacrolimus' }] },
+  ]
+
+  return <div></div>
 }
