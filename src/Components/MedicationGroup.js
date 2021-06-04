0117124e2f23da286510e1682e7231ee5522f03c
Annemarie Wisbar <a.wisbar@gmail.com>
2021-06-04 16:38:50 +0200

    Update styles in MedicationGroup

--- src/Components/MedicationGroup.js
+++ src/Components/MedicationGroup.js
@@ -11,7 +11,7 @@ MedicationGroup.propTypes = {
 export default function MedicationGroup({ time, meds }) {
   return (
     <Wrapper>
-      <time dateTime={time}>{time} UHR</time>
+      <Time dateTime={time}>{time} UHR</Time>
       <Meds>
         {meds.map(({ id, medName }) => (
           <li key={id}>{medName}</li>
@@ -24,15 +24,23 @@ export default function MedicationGroup({ time, meds }) {
 const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
+  gap: 12px;
   justify-items: space-between;
-  padding: 20px;
+  border: 2px solid grey;
   border-radius: 8px;
+  padding: 12px;
   box-shadow: 0 8px 16px var(--color-shadow);
+`
+
+const Time = styled.time`
+  font-size: 1.3em;
 `
 
 const Meds = styled.ul`
   display: flex;
   flex-direction: column;
+  gap: 4px;
   list-style: none;
   padding: 0;
+  margin: 0;
 `
