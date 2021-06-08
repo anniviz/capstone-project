3d62ac040109833a07ab71092ec56e6d0b1b1c54
Annemarie Wisbar <a.wisbar@gmail.com>
2021-06-08 11:41:27 +0200

    Add tests for TextButton

---
+++ src/Components/TextButton.spec.js
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import TextButton from './TextButton'
+
+describe('Button', () => {
+  it('contains a text', () => {
+    render(<TextButton onClick={() => {}}>Click me</TextButton>)
+
+    const button = screen.getByRole('button', { name: 'Click me' })
+    expect(button).toBeInTheDocument()
+  })
+
+  it('calls onClick', () => {
+    const handleClick = jest.fn()
+    render(<TextButton onClick={handleClick}>Click me</TextButton>)
+
+    const button = screen.getByRole('button', { name: 'Click me' })
+    userEvent.click(button)
+
+    expect(handleClick).toHaveBeenCalledTimes(1)
+  })
+})
