1cf900090231dfac5b730759513e6be14a24fa45
Annemarie Wisbar <a.wisbar@gmail.com>
2021-06-08 14:35:01 +0200

    Add FormPage to App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import { useState } from 'react'
-import styled from 'styled-components/macro'
-import Header from './components/Header'
-import MedicationGroup from './components/MedicationGroup'
-import Form from './pages/FormPage'
+import FormPage from './pages/FormPage'
+import MedicationPage from './pages/MedicationPage'
 
 export default function App() {
-  const currentDate = new Date()
-
   const [medications, setMedications] = useState([])
-  const [activePage, setActivePage] = useState('form')
+  const [activePage, setActivePage] = useState('medication')
 
   return (
     <>
       {activePage === 'medication' && (
-        <Grid>
-          <Header>{formatDate(currentDate)}</Header>
-          <Flexbox>
-            {medications.map(({ id, time, meds }) => (
-              <MedicationGroup key={id} time={time} meds={meds} />
-            ))}
-          </Flexbox>
-        </Grid>
+        <MedicationPage
+          medications={medications}
+          setActivePage={setActivePage}
+        />
       )}
       {activePage === 'form' && (
-        <Form
+        <FormPage
           onClick={handleActivePage}
           setActivePage={setActivePage}
           onSubmit={handleSubmit}
@@ -39,51 +31,4 @@ export default function App() {
   function handleSubmit(newMedication) {
     setMedications([newMedication, ...medications])
   }
-
-  function formatDate(date) {
-    const days = [
-      'Sonntag',
-      'Montag',
-      'Dienstag',
-      'Mittwoch',
-      'Donnerstag',
-      'Freitag',
-      'Samstag',
-    ]
-
-    const months = [
-      'Januar',
-      'Februar',
-      'März',
-      'April',
-      'Mai',
-      'Juni',
-      'Juli',
-      'August',
-      'September',
-      'Oktober',
-      'November',
-      'Dezember',
-    ]
-
-    const formatedDate = `${days[
-      date.getDay()
-    ].toUpperCase()}, ${date.getDate()}. ${months[date.getMonth()]}`
-
-    return formatedDate
-  }
 }
-
-const Grid = styled.div`
-  height: 100vh;
-  display: grid;
-  grid-template-rows: 60px 1fr;
-`
-
-const Flexbox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  padding: 16px;
-  overflow-y: auto;
-`
