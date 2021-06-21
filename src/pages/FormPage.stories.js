71a98ec60216d2f24d021cab2b1a53dfddd5774a
Annemarie Wisbar <a.wisbar@gmail.com>
2021-06-21 17:32:14 +0200

    Update storybook

--- src/pages/FormPage.stories.js
+++ src/pages/FormPage.stories.js
@@ -4,25 +4,37 @@ import FormPage from './FormPage'
 export default {
   title: 'pages/Form',
   component: FormPage,
-  argTypes: { setActivePage: { action: 'setActivePage' } },
+  argTypes: {
+    setActivePage: { action: 'setActivePage' },
+    isDisabled: { type: 'boolean' },
+  },
 }
 
 export const Default = args => <FormPage {...args} />
 Default.args = {
-  today: 'MONTAG, 7. JUNI',
-  medicationToEdit: {},
+  selectedDay: new Date(),
+  medications: [],
+  medicationToEditId: null,
 }
 
 export const Edit = args => <FormPage {...args} />
 Edit.args = {
-  today: 'MONTAG, 7. JUNI',
-  medicationToEdit: {
-    id: '01234',
-    time: '8:00',
-    meds: [
-      { id: '01234', medName: 'ASS' },
-      { id: '01235', medName: 'Metoprolol' },
-      { id: '01236', medName: 'Magnesium' },
-    ],
-  },
+  selectedDay: new Date(),
+  medications: [
+    {
+      id: '01234',
+      time: '8:00',
+      meds: [
+        { id: '01234', medName: 'ASS' },
+        { id: '01235', medName: 'Metoprolol' },
+        { id: '01236', medName: 'Magnesium' },
+      ],
+    },
+    {
+      id: '01235',
+      time: '9:00',
+      meds: [{ id: '01234', medName: 'Tacrolimus' }],
+    },
+  ],
+  medicationToEditId: '01234',
 }
