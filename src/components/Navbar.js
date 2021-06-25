faff58b2068182e7dfe87eeae6cdda90c5a395ea
Annemarie Wisbar <a.wisbar@gmail.com>
2021-06-25 11:06:08 +0200

    Change color of nav icons on click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,30 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import styled from 'styled-components/macro'
 import AddLink from './AddLink'
 import medicationsDisabled from '../icons/pill-disabled.svg'
+import medicationsEnabled from '../icons/pill-enabled.svg'
 import observationsDisabled from '../icons/observations-disabled.svg'
+import observationsEnabled from '../icons/observations-enabled.svg'
 
 export default function Navbar() {
+  const location = useLocation()
+
   return (
     <Nav>
-      <NavLink exact to="/medications">
-        <img src={medicationsDisabled} alt="" height="32px" />
-      </NavLink>
-      <NavLink exact to="/observations">
-        <img src={observationsDisabled} alt="" height="32px" />
-      </NavLink>
+      <NavLinkStyled exact to="/medications">
+        {location.pathname.includes('medications') ? (
+          <img src={medicationsEnabled} alt="" height="32px" />
+        ) : (
+          <img src={medicationsDisabled} alt="" height="32px" />
+        )}
+      </NavLinkStyled>
+      <NavLinkStyled exact to="/observations">
+        {location.pathname.includes('observations') ? (
+          <img src={observationsEnabled} alt="" height="32px" />
+        ) : (
+          <img src={observationsDisabled} alt="" height="32px" />
+        )}
+      </NavLinkStyled>
       <AddLink to="/medications/form"></AddLink>
     </Nav>
   )
@@ -27,4 +39,8 @@ const Nav = styled.nav`
   background-color: var(--color-basis);
   box-shadow: 26px 26px 68px var(--color-shadow-21);
   position: relative;
+`
+
+const NavLinkStyled = styled(NavLink)`
+  cursor: default;
 `
