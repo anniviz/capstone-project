91ac373d04b70986855ab57ef6ac3522d57045d8
Annemarie Wisbar <a.wisbar@gmail.com>
2021-06-28 12:12:47 +0200

    Bug fixing

--- src/pages/observationPages/ObservationFormPageDefault.stories.js
+++ src/pages/observationPages/ObservationFormPageDefault.stories.js
@@ -24,4 +24,16 @@ export default {
 }
 
 export const Default = args => <ObservationFormPageDefault {...args} />
-Default.args = {}
+Default.args = {
+  observationTypes: [
+    { name: 'Größe', type: 'size', unit: 'cm' },
+    { name: 'Gewicht', type: 'weight', unit: 'kg' },
+    { name: 'Temperatur', type: 'temperature', unit: '°C' },
+    { name: 'Blutdruck', type: 'bloodpressure', unit: 'mmHg' },
+    { name: 'FEV1', type: 'fev1', unit: 'l/s' },
+    { name: 'Blutzucker', type: 'bloodsugar', unit: 'mmol/l' },
+    { name: 'Urin', type: 'urin', unit: '' },
+    { name: 'Notizen', type: 'notes', unit: '' },
+  ],
+  observationType: 'fev1',
+}
