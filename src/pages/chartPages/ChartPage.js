0106d40bbe9d7d408a19716bcd1c1050512373c9
Annemarie Wisbar <a.wisbar@gmail.com>
2021-06-30 01:42:04 +0200

    Sort observationsDiary by date

--- src/pages/chartPages/ChartPage.js
+++ src/pages/chartPages/ChartPage.js
@@ -1,7 +1,17 @@
 import styled from 'styled-components/macro'
 import * as d3 from 'd3'
 
-export default function ChartPage(observationsDiary) {
+export default function ChartPage({ observationsDiary }) {
+  const formatTime = d3.timeFormat('%Y-%m-%d')
+  const parseTime = d3.timeParse('%Y-%m-%d')
+  const sortedObbservationsDiary = observationsDiary
+    .slice()
+    .sort((a, b) => parseTime(a.date).getTime() - parseTime(b.date).getTime())
+  console.log(observationsDiary)
+  console.log(sortedObbservationsDiary)
+  // const xScale = d3.scaleTime().domain([new Date(d3.min(observationsDiary.))])
+  // .range([0, 200])
+  // .nice()
   return (
     <Grid>
       <Canvas></Canvas>
