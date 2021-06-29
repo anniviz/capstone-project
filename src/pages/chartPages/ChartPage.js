c2369d43636bd171e8a0091e0aaee14d9a0513b0
Annemarie Wisbar <a.wisbar@gmail.com>
2021-06-30 01:28:16 +0200

    Add Canvas to ChartPage

--- src/pages/chartPages/ChartPage.js
+++ src/pages/chartPages/ChartPage.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components/macro'
+import * as d3 from 'd3'
 
 export default function ChartPage() {
-  return <Grid></Grid>
+  return (
+    <Grid>
+      <Canvas></Canvas>
+    </Grid>
+  )
 }
 
 const Grid = styled.main`
@@ -9,4 +14,9 @@ const Grid = styled.main`
   grid-template-rows: ${props =>
     props.showCalendar ? 'auto auto 1fr' : 'auto 1fr'};
   overflow: auto;
+`
+
+const Canvas = styled.svg`
+  width: 100%;
+  height: 400px;
 `
