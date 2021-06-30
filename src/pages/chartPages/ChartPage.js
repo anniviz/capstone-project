1eb05394ebf1bf7e7e453f769e2b3fa8bdfd3b12
Annemarie Wisbar <a.wisbar@gmail.com>
2021-06-30 14:36:30 +0200

    Add basic styling

--- src/pages/chartPages/ChartPage.js
+++ src/pages/chartPages/ChartPage.js
@@ -3,7 +3,7 @@ import * as d3 from 'd3'
 import { useEffect, useRef, useState } from 'react'
 
 export default function ChartPage({ observationsDiary, observationType }) {
-  const xAxisTickFormat = d3.timeFormat('%d.%m')
+  const xAxisTickFormat = d3.timeFormat('%d.%m.%y')
   const parseTime = d3.timeParse('%Y-%m-%d')
 
   const canvasRef = useRef(null)
@@ -17,10 +17,11 @@ export default function ChartPage({ observationsDiary, observationType }) {
     setHeight(canvasRef.current.getBoundingClientRect().height)
   }, [canvasRef])
 
-  const margin = { top: 20, right: 30, bottom: 40, left: 20 }
+  const margin = { top: 20, right: 30, bottom: 80, left: 40 }
 
   const innerWidth = width - margin.left - margin.right
   const innerHeight = height - margin.top - margin.bottom
+  console.log(innerHeight)
 
   const xAxisLabelOffset = 50
   const yAxisLabelOffset = 45
@@ -62,33 +63,33 @@ export default function ChartPage({ observationsDiary, observationType }) {
     <Grid>
       <Canvas ref={canvasRef}>
         <Chart marginLeft={margin.left} marginTop={margin.top}>
-          <path fill="none" stroke="black" d={line(ObservationValueArray)} />
-          {ObservationValueArray.map(day => (
+          {/* {ObservationValueArray.map(day => (
             <circle
               key={day.date}
               cx={xScale(day.date)}
               cy={yScale(day.observationValue)}
               r="3"
             />
-          ))}
+          ))} */}
           {xScale.ticks(7).map(tickValue => (
-            <>
-              <g
-                className="tick"
-                key={tickValue}
-                transform={`translate(${xScale(tickValue)},0)`}
+            <g
+              className="tick"
+              key={tickValue}
+              transform={`translate(${xScale(tickValue)},0)`}
+            >
+              <line x1="0" y1="0" x2="0" y2={innerHeight} stroke="lightgrey" />
+              <TickMarksBottom
+                dy=".71em"
+                y={innerHeight + 7}
+                translate={xScale(tickValue)}
+                transformOriginX={margin.left}
+                transformOriginY={innerHeight}
               >
-                <line y2={innerHeight} />
-                <TickMarksBottom
-                  dy=".71em"
-                  y={innerHeight + 7}
-                  translate={xScale(tickValue)}
-                >
-                  {xAxisTickFormat(tickValue)}
-                </TickMarksBottom>
-              </g>
-            </>
+                {xAxisTickFormat(tickValue)}
+              </TickMarksBottom>
+            </g>
           ))}
+          <Line d={line(ObservationValueArray)} />
         </Chart>
       </Canvas>
     </Grid>
@@ -113,8 +114,14 @@ const Chart = styled.g`
   );
 `
 
+const Line = styled.path`
+  fill: none;
+  stroke: var(--color-tertiary);
+`
+
 const TickMarksBottom = styled.text`
   transform: rotate(-65deg);
-  transform-origin: bottom left;
-  text-anchor: start;
+  transform-origin: 0px ${props => props.transformOriginY}px;
+  text-anchor: end;
+  font-size: 0.8em;
 `
